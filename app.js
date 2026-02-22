a37c934b52e980e59aaa7d3eaf05f36879fb846e
gumol <jacoszekjacoszek@o2.pl>
2026-02-22 20:00:40 +0100

    overlay end

---
+++ app.js
@@ -0,0 +1,15 @@
+'use strict'
+
+// modal variables
+const modal = document.querySelector('[data-modal]')
+const modalCloseBtn = document.querySelector('[data-modal-close]')
+const modalCloseOverlay = document.querySelector('[data-modal-overlay]')
+
+// modal function
+const modalCloseFunc = () => {
+    modal.classList.add('closed')
+}
+
+// modale eventListener
+modalCloseOverlay.addEventListener('click',modalCloseFunc)
+modalCloseBtn.addEventListener('click',modalCloseFunc)
