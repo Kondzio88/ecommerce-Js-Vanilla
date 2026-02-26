0cab6c8d797d648973d41f414e983afb1b84897c
gumol <jacoszekjacoszek@o2.pl>
2026-02-26 13:33:50 +0100

    end header and category section

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ const modalCloseFunc = () => {
 modalCloseOverlay.addEventListener('click', modalCloseFunc)
 modalCloseBtn.addEventListener('click', modalCloseFunc)
 
+// ----------------------------------------------------------------------------//
+
 // notification toast variables
 
 const notificationToast = document.querySelector('[data-toast]')
@@ -26,29 +28,35 @@ toatsCloseBtn.addEventListener('click', () => {
     notificationToast.classList.add('closed')
 })
 
+// ----------------------------------------------------------------------------//
+
+
 // mobile menu variables
 
 const mobileMenuOpenBtn = document.querySelectorAll('[data-mobile-menu-open-btn]')
-const mobileMenu = document.querySelectorAll('[data-mobile-menu]')
+const mobileMenu = document.querySelector('[data-mobile-menu]')
 const mobileMenuCloseBtn = document.querySelectorAll('[data-mobile-menu-close-btn]')
 const overlay = document.querySelector('[data-overlay]')
 
+// mobile menu func
 
-for (let i = 0; i < mobileMenuOpenBtn.length; i++) {
+const mobileMenuCloseFunc = () => {
+    mobileMenu.classList.remove('active');
+    overlay.classList.remove('active');
+};
 
-    const mobileMenuCloseFunc = () => {
-        mobileMenu[i].classList.remove('active')
-        overlay.classList.remove('active')
-    }
-
-    mobileMenuOpenBtn[i].addEventListener('click', () => {
-        mobileMenu[i].classList.add('active')
-        overlay.classList.add('active')
-    })
-
-    mobileMenuCloseBtn[i].addEventListener('click', mobileMenuCloseFunc)
-    overlay.addEventListener('click', mobileMenuCloseFunc)
+const mobileMenuOpenFunc = () => {
+    mobileMenu.classList.add('active');
+    overlay.classList.add('active');
 }
+
+mobileMenuCloseBtn.forEach(btn => {
+    btn.addEventListener('click', mobileMenuCloseFunc)
+})
+
+mobileMenuOpenBtn.forEach(btn => {
+    btn.addEventListener('click', mobileMenuOpenFunc)
+})
 
 // accordion variables
 
@@ -56,27 +64,28 @@ const accordionBtn = document.querySelectorAll('[data-accordion-btn]')
 const accordion = document.querySelectorAll('[data-accordion]')
 console.log(accordionBtn, accordion);
 
-for (let i = 0; i < accordionBtn.length; i++) {
+accordionBtn.forEach(btn => {
+    btn.addEventListener('click', function () {
 
-    accordionBtn[i].addEventListener('click', () => {
-        const clickedBtn = this.nextElementSibling.classList.contains('active')
+        const subMenu = this.nextElementSibling
 
-        for (let i = 0; i < accordion.length; i++) {
+        const isClosed = !this.classList.contains('active')
+        console.log(this);
+        accordionBtn.forEach(otherBtn => {
+            otherBtn.classList.remove('active')
+            otherBtn.nextElementSibling.classList.remove('active')
+        })
 
-            if (clickedBtn) break
-
-            if (accordion[i].classList.contains('active')) {
-
-                accordion[i].classList.remove('active')
-                accordionBtn[i].classList.remove('active')
-            }
+        if (isClosed) {
+            subMenu.classList.add('active')
+            this.classList.add('active')
         }
 
-        this.nextElementSibling.classList.toggle('active')
-        this.classList.toggle('active')
     })
+})
 
-}
+
+// ----------------------------------------------------------------------------//
 
 // fake Api
 
@@ -85,7 +94,6 @@ async function pobierzBaze() {
     try {
         const response = await fetch('./db.json');
         const result = await response.json();
-        console.log(result);
     } catch (error) {
         console.error(error);
     }
